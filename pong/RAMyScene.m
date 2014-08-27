69ff625438d9b346650a29bca1203e9f9ddaf3ac
naturaln0va <naturaln0va@Ryans-MacBook-Pro.local>
2014-08-27 13:17:31 -0700

    Removed Unnecessary Files & Organized

--- pong/RAMyScene.m
+++ pong/RAMyScene.m
@@ -40,6 +40,19 @@ static inline CGFloat ScalarRandomRange(CGFloat min, CGFloat max){
     int _score;
 }
 
+-(id)initWithSize:(CGSize)size {    
+    if (self = [super initWithSize:size]) {
+        _worldNode = [SKNode node];
+        
+        [self createPlayerPaddle];
+        
+        [self addChild:_worldNode];
+    }
+    return self;
+}
+
+#pragma mark - Init
+
 - (void)createPlayerPaddle {
     self.backgroundColor = [SKColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:1.0];
     
@@ -58,25 +71,26 @@ static inline CGFloat ScalarRandomRange(CGFloat min, CGFloat max){
     [_worldNode addChild:_playerPaddle];
 }
 
--(id)initWithSize:(CGSize)size {    
-    if (self = [super initWithSize:size]) {
-        _worldNode = [SKNode node];
-        
-        [self createPlayerPaddle];
-        
-        [self addChild:_worldNode];
-    }
-    return self;
-}
+#pragma mark - Touches
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
     
     /*for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-    }*/
+     CGPoint location = [touch locationInNode:self];
+     
+     }*/
 }
+
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+}
+
+-(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+}
+
+#pragma mark - Update
 
 -(void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
